79e51b293b21d0fd072439a5b329de44b466e5ca
ChrisChrisLoLo <sporewoh@gmail.com>
2020-11-21 21:16:12 -0700

    Updates

---
+++ precache-manifest.cde6d6aec701b375dc315cde5e237933.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c00401a84d607d4df57554edb6775c0",
+    "url": "/GomokuOnline/index.html"
+  },
+  {
+    "revision": "8a3b7bbe6c0b58a28b94",
+    "url": "/GomokuOnline/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5b24d715e9982ffb425e",
+    "url": "/GomokuOnline/static/css/main.681a6456.chunk.css"
+  },
+  {
+    "revision": "8a3b7bbe6c0b58a28b94",
+    "url": "/GomokuOnline/static/js/2.09cbc09d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/GomokuOnline/static/js/2.09cbc09d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b24d715e9982ffb425e",
+    "url": "/GomokuOnline/static/js/main.2a9b1021.chunk.js"
+  },
+  {
+    "revision": "8b1466c25d32dc2ed6fe",
+    "url": "/GomokuOnline/static/js/runtime-main.0c97c4f7.js"
+  },
+  {
+    "revision": "d46edd4e96958904264c02546c9cd83e",
+    "url": "/GomokuOnline/static/media/black.d46edd4e.svg"
+  },
+  {
+    "revision": "c7808d18a16f9ac677df042b6b3f9a7a",
+    "url": "/GomokuOnline/static/media/white.c7808d18.svg"
+  }
+]);
